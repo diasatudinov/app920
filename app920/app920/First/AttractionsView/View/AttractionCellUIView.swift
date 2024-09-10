db0569bd44ef08b3b9ee614863c5d47fd5275b6b
Dias <dias.atudinov@gmail.com>
2024-09-10 18:05:30 +0500

    Attraction Cell

--- app920/app920/First/AttractionsView/View/AttractionCellUIView.swift
+++ app920/app920/First/AttractionsView/View/AttractionCellUIView.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct AttractionCellUIView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black
+            VStack {
+                Image("firstScreen920")
+                    .resizable()
+                    .frame(height: 130)
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Louvre")
+                            .font(.system(size: 17, weight: .semibold))
+                        Text("France, Paris")
+                            .font(.system(size: 15))
+                    }.foregroundColor(.white).padding(.bottom)
+                    Spacer()
+                    VStack(alignment: .trailing) {
+                        Text("30 $")
+                            .font(.system(size: 16, weight: .semibold))
+                        Text("Historical").foregroundColor(.red)
+                            .font(.system(size: 15))
+                            .italic()
+                    }.foregroundColor(.white).padding(.bottom)
+                }.frame(maxHeight: .infinity).padding(.horizontal)
+            }
+        }.frame(height: 186).cornerRadius(10)
     }
 }
 
