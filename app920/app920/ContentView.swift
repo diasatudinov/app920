d7677c7174a2b2df4c66765bb029c93caae712af
Dias <dias.atudinov@gmail.com>
2024-09-10 13:49:43 +0500

    Onboarding Views 1

---
+++ app920/app920/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  app920
+//
+//  Created by Dias Atudinov on 10.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app920/app920/app920App.swift
@@ -0,0 +1,17 @@
+//
+//  app920App.swift
+//  app920
+//
+//  Created by Dias Atudinov on 10.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct app920App: App {
+    var body: some Scene {
+        WindowGroup {
+            LoaderUIView()
+        }
+    }
+}
