3199219e1967476979b143191c6b826a0099a8e2
Dias <dias.atudinov@gmail.com>
2024-09-10 14:26:45 +0500

    Home View empty view

--- app920/app920/First/HomeView/View/HomeUIView.swift
+++ app920/app920/First/HomeView/View/HomeUIView.swift
@@ -9,7 +9,64 @@ import SwiftUI
 
 struct HomeUIView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.mainBg.ignoresSafeArea()
+            VStack {
+                HStack {
+                    ZStack {
+                        Image(systemName: "person.fill")
+                            .font(.system(size: 32))
+                            .foregroundColor(.red)
+                            .padding(.top)
+                            .background(Color.white.opacity(0.5)).clipShape(Circle())
+                            .padding(9)
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.white.opacity(0.5), lineWidth: 1)
+                            )
+                    }
+                    
+                    Text("Welcome Nina!")
+                        .foregroundColor(.white)
+                        .font(.system(size: 20, weight: .semibold))
+                    Spacer()
+                }
+                
+                if true {
+                    VStack(spacing: 10) {
+                        Image("emptyLogo")
+                            .padding(.bottom, 10)
+                        
+                        Text("The list is empty")
+                            .font(.system(size: 16, weight: .semibold))
+                            .foregroundColor(.white)
+                        Text("Add new points of interest by clicking \non the button")
+                            .multilineTextAlignment(.center)
+                            .font(.system(size: 15))
+                            .foregroundColor(.white)
+                            .padding(.bottom, 10)
+                        
+                        Button {
+                            
+                        } label: {
+                            ZStack {
+                                Rectangle()
+                                    .frame(height: 50)
+                                    .cornerRadius(16)
+                                    .foregroundColor(.redBtn)
+                                Text("Add point of interest")
+                                    .font(.system(size: 15))
+                                    .foregroundColor(.white)
+                            }
+                        }
+                    }.padding(.top, 50)
+                } else {
+                    
+                }
+                
+                Spacer()
+            }.padding(.horizontal)
+        }
     }
 }
 
--- app920/app920/First/TabBar/TabUIView.swift
+++ app920/app920/First/TabBar/TabUIView.swift
@@ -8,8 +8,86 @@
 import SwiftUI
 
 struct TabUIView: View {
+    @State var selectedTab = 0
+    private let tabs = ["Home", "Attractions", "Memories", "Settings"]
+    
+//    @ObservedObject var homeVM = HomeViewModel()
+//    @ObservedObject var procedureVM = ProcedureViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            switch selectedTab {
+            case 0:
+                HomeUIView()
+                    
+            case 1:
+                Text("Second")
+
+            case 2:
+                Text("Third")
+            case 3:
+                Text("Fourth")
+            default:
+                Text("default")
+            }
+                VStack {
+                    Spacer()
+                    
+                    ZStack {
+                        Rectangle()
+                            .fill(Color.mainBg)
+                            .frame(height: 80)
+                            
+                        HStack(spacing: 50) {
+                            ForEach(0..<tabs.count) { index in
+                                Button(action: {
+                                    selectedTab = index
+                                }) {
+                                    
+                                    ZStack {
+                                        VStack {
+                                            Image(systemName: icon(for: index))
+                                                .font(.system(size: 20, weight: .semibold))
+                                                .padding(.bottom, 2)
+                                            Text(text(for: index))
+                                                .font(.system(size: 10, weight: .semibold))
+                                        }.foregroundColor(selectedTab == index ? Color.redBtn : Color.gray)
+                                    }
+                                }
+                                
+                            }
+                        }.padding(.bottom, 5)
+                        
+                        Rectangle()
+                            .fill(Color.black)
+                            .frame(height: 1)
+                            .padding(.bottom, 78)
+                    }
+                    
+                }.ignoresSafeArea()
+            
+        }
+    }
+    
+    private func icon(for index: Int) -> String {
+        switch index {
+        case 0: return "house.fill"
+        case 1: return "bus"
+        case 2: return "camera.fill"
+        case 3: return "gear"
+        default: return ""
+        }
+    }
+    
+    private func text(for index: Int) -> String {
+        switch index {
+        case 0: return "Home"
+        case 1: return "Attractions"
+        case 2: return "Memories"
+        case 3: return "Settings"
+        default: return ""
+        }
     }
 }
 
