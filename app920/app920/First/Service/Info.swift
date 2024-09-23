79a0991bc48b95ef81ea59f58c9d350dd6392c49
Dias <dias.atudinov@gmail.com>
2024-09-23 13:28:28 +0500

    Use new data about app

--- app920/app920/First/Service/Info.swift
+++ app920/app920/First/Service/Info.swift
@@ -9,10 +9,6 @@ import Foundation
 
 struct Info {
     
-    let oneZero: String = "appcloudworks.fun/app/g0lfst4tw1nguru"
-    let appHudID = "app_sxf4uuh5JV1JWyNJwwYi3U8PWJ83bN"
-    let usagePolicy = "https://www.termsfeed.com/live/61db5db7-6501-43e9-8422-5c4aa3e6ddd0"
-    let oneSignal = "c34029a9-cd0d-4816-8858-0a88e5977723"
-    let techCode: String = "can_update"
+    let usagePolicy = "https://www.termsfeed.com/live/56dc2018-6ba9-4055-9721-0a338017abba"
     
 }
