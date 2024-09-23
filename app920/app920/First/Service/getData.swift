5619aa2589141b1343652f04d27846feafd5ccc9
Dias <dias.atudinov@gmail.com>
2024-09-23 13:27:51 +0500

    New files for Web View

---
+++ app920/app920/First/Service/getData.swift
@@ -0,0 +1,8 @@
+//
+//  getData.swift
+//  app920
+//
+//  Created by Dias Atudinov on 23.09.2024.
+//
+
+import Foundation
---
+++ app920/app920/Second/WebView/WebUIView.swift
@@ -0,0 +1,18 @@
+//
+//  WebUIView.swift
+//  app920
+//
+//  Created by Dias Atudinov on 23.09.2024.
+//
+
+import SwiftUI
+
+struct WebUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebUIView()
+}
